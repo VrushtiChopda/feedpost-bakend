64b7392eb0ecccda913581e7d1cacc13f7ba1b54
VrushtiChopda <sivrushti@gmail.com>
2024-09-06 18:10:03 +0530

    edit code

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -8,6 +8,6 @@ const { authMiddleware } = require('../middleware/auth.middleware')
 router.post('/addComment', authMiddleware, validate(addCommentValidaton), addComment)
 router.get('/getComment', authMiddleware, getComment)
 router.get('/getCommentById/:id', getCommentByPostId)
-router.put('/updateComment/:id', authMiddleware, validate(updateCommentValidation), updateComment)
+router.put('/updateComment/:id', authMiddleware, validate(commentId), validate(updateCommentValidation), updateComment)
 router.delete('/deleteComment/:id', authMiddleware, validate(commentId), deleteComment)
 module.exports = router
